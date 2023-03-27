a9c1fa05a04e54dc2958b150e338e7d3b5212950
kamenifrancis <blickson57@gmail.com>
2023-03-27 16:19:19 +0100

    react js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import ColorSchemesExample from './Navbar';
+import './App.css';
+import KitchenSinkExample from './Carte';
+
+function App() {
+  return (
+    <div className="App">
+  <ColorSchemesExample/> 
+  <KitchenSinkExample/>
+    </div>
+  );
+}
+
+export default App;
